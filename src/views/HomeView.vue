2a85d0b9f688397837677fe5aba1b279edbbe25b
nashPOP <54317907+nashPOP@users.noreply.github.com>
2023-08-08 15:58:00 +0800

    hide backbutton in home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
-// import {BackButton } from '@twa.js/sdk';
+import {BackButton } from '@twa.js/sdk';
 
-// const backbutton = new BackButton("6.2");
-// backbutton.hide()
+const backbutton = new BackButton("6.2");
+backbutton.hide()
 
 </script>
 <template>
