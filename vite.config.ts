23450a32790043d7b7cadab640ef383fc2734090
nashPOP <54317907+nashPOP@users.noreply.github.com>
2023-08-08 14:55:35 +0800

    vue+vite+ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { fileURLToPath, URL } from 'node:url'
+const path = require('path')
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//          '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+//     },
+
+//   }
+// })
+
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  base: '/my-twa/',
-});
+  // root: path.resolve(__dirname, 'src'),
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    // hot: true
+  }
+})
