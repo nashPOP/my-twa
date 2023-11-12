12d17f7ff29c7933b1e9cadbdf82da62674c435e
nashPOP <54317907+nashPOP@users.noreply.github.com>
2023-11-13 02:04:55 +0800

    update connector.js

--- src/utils/connector.js
+++ src/utils/connector.js
@@ -1,6 +1,6 @@
 import { TonConnect } from "@tonconnect/sdk";
 const dappMetadata = {
-  manifestUrl: "https://demo.tondrive.io/tonconnect-manifest.json",
+  manifestUrl: "https://nashpop.github.io/my-twa/tonconnect-testnet-manifest.json",
 };
 
 export const connector = new TonConnect(dappMetadata);
